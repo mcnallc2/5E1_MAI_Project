e5be9375affe1e37dca28c48eb63a8c9ecfac585
mcnallc2 <conormcnally247@gmail.com>
2020-12-04 00:01:30 +0000

    sending coordinates to pynq board

--- pi/rplidar_sdk/app/ultra_simple/main.cpp
+++ pi/rplidar_sdk/app/ultra_simple/main.cpp
@@ -25,8 +25,11 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cmath>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 
 #include <wiringPi.h>
@@ -45,81 +48,15 @@
 // rplidar namespace
 using namespace rp::standalone::rplidar;
 
+
 // create runtime delay
-static inline void delay(_word_size_t ms){
-    while (ms>=1000){
-        usleep(1000*1000);
-        ms-=1000;
-    };
-    if (ms!=0)
-        usleep(ms*1000);
-}
-
+static inline void delay(_word_size_t ms);
 // checking lidar health status
-bool checkRPLIDARHealth(RPlidarDriver * drv){
-    //
-    u_result op_result;
-    rplidar_response_device_health_t healthinfo;
-    //
-    // obtain health info
-    op_result = drv->getHealth(healthinfo);
-    //
-    // macro to compare with a failure bit to check for success
-    if (IS_OK(op_result)) {
-        //
-        // printf("RPLidar health status : %d\n", healthinfo.status);
-        //
-        // if status is 0x2 there is an error
-        if (healthinfo.status == RPLIDAR_STATUS_ERROR) {
-            fprintf(stderr, "Error, rplidar internal error detected. Please reboot the device to retry.\n");
-            //
-            //using driver to reboot the lidar
-            drv->reset();
-            return false;
-        }
-        //
-        // if no error return true
-        else{
-            return true;
-        }
-    }
-    // 
-    // if cannot get lidar health status
-    else{
-        fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
-        return false;
-    }
-}
-
+bool checkRPLIDARHealth(RPlidarDriver * drv);
 // get the average distance for the angle range
-void getDistanceforAngle(rplidar_response_measurement_node_hq_t * nodebuffer, size_t count, int lower, int upper){
-    //
-    float distance = 0;
-    float angle;
-    //
-    // loop through the lidar range of angles
-    for (int pos = 0; pos < (int)count ; ++pos){
-        //
-        // get angle in degrees
-        angle = (nodebuffer[pos].angle_z_q14 * 90.f / (1 << 14));
-        //
-        // if angle is within the range and the info is good quality
-        if ((angle >= lower) && (angle < upper) && (nodebuffer[pos].quality != 0)){
-            //
-            // for the first read the distance is 0, store the first distance
-            if (distance == 0){
-                distance = nodebuffer[pos].dist_mm_q2/4.0f;
-            }
-            //
-            // else add the new distance to the average value
-            else{
-                distance = (distance + nodebuffer[pos].dist_mm_q2/4.0f) / 2;                        
-            }
-        }
-    }
-    // print the average ditance for the angle range
-    printf("Angle: %i-%i\t- Dist: %08.2f\n", lower, upper, distance);
-}
+void getDistanceforAngle(int serial_port, rplidar_response_measurement_node_hq_t * nodebuffer, size_t count, int spec_angle);
+// get the coordinate for the distance at each angle
+void getCoordinatetoSend(int serial_port, int spec_angle, float distance);
 
 // end when ctrl_c is pressed
 bool ctrl_c_pressed;
@@ -130,8 +67,8 @@ void ctrlc(int){
 
 int main(int argc, const char * argv[]){
     //
-    int serial_port ;
-    char dat;
+    int serial_port;
+    //
     if ((serial_port = serialOpen ("/dev/ttyS0", 115200)) < 0){   /* open serial port */
         fprintf(stderr, "Unable to open serial device: %s\n", strerror (errno));
         return 1 ;
@@ -231,18 +168,12 @@ int main(int argc, const char * argv[]){
             // Starting: 160
             // Finish: 180
             // Width: 5
-            for(int range = 160; range < 175; range=range+5){
+            for(int angle = 90; angle < 270; angle++){
                 //
-                // get the average distance value for range
-                getDistanceforAngle(nodes, count, range, range+5);
+                getDistanceforAngle(serial_port, nodes, count, angle);
+                //
             }
-            printf("\n");
         }
-        //
-        dat = 'A';
-        printf("Sending CHAR - %c\n", dat);
-        fflush(stdout);
-        serialPutchar(serial_port, dat);    /* transmit character serially on port */
         //
         // break if ctrl_c is pressed
         if (ctrl_c_pressed){ 
@@ -262,3 +193,143 @@ on_finished:
     return 0;
 }
 
+// create runtime delay
+static inline void delay(_word_size_t ms){
+    while (ms>=1000){
+        usleep(1000*1000);
+        ms-=1000;
+    };
+    if (ms!=0)
+        usleep(ms*1000);
+}
+
+// checking lidar health status
+bool checkRPLIDARHealth(RPlidarDriver * drv){
+    //
+    u_result op_result;
+    rplidar_response_device_health_t healthinfo;
+    //
+    // obtain health info
+    op_result = drv->getHealth(healthinfo);
+    //
+    // macro to compare with a failure bit to check for success
+    if (IS_OK(op_result)) {
+        //
+        // printf("RPLidar health status : %d\n", healthinfo.status);
+        //
+        // if status is 0x2 there is an error
+        if (healthinfo.status == RPLIDAR_STATUS_ERROR) {
+            fprintf(stderr, "Error, rplidar internal error detected. Please reboot the device to retry.\n");
+            //
+            //using driver to reboot the lidar
+            drv->reset();
+            return false;
+        }
+        //
+        // if no error return true
+        else{
+            return true;
+        }
+    }
+    // 
+    // if cannot get lidar health status
+    else{
+        fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
+        return false;
+    }
+}
+
+// get the average distance for the angle range
+void getDistanceforAngle(int serial_port, rplidar_response_measurement_node_hq_t * nodebuffer, size_t count, int spec_angle){
+    //
+    int distance = 0;
+    float angle;
+    //
+    // loop through the lidar range of angles
+    for (int pos = 0; pos < (int)count ; ++pos){
+        //
+        // get angle in degrees
+        angle = (nodebuffer[pos].angle_z_q14 * 90.f / (1 << 14));
+        //
+        // if angle is within the range and the info is good quality
+        if ((angle >= spec_angle-0.5) && (angle < spec_angle+0.5) && (nodebuffer[pos].quality != 0)){
+            //
+            // for the first read the distance is 0, store the first distance
+            if (distance == 0){
+                distance = nodebuffer[pos].dist_mm_q2/4.0f;
+            }
+            //
+            // else add the new distance to the average value
+            else{
+                distance = (distance + nodebuffer[pos].dist_mm_q2/4.0f) / 2;                        
+            }
+        }
+    }
+    //
+    if(distance){
+        getCoordinatetoSend(serial_port, spec_angle-90, distance);
+    }
+    //
+}
+
+// get the coordinate for the distance at each angle
+void getCoordinatetoSend(int serial_port, int spec_angle, float distance){
+    //
+    int x_width = 150;
+    float x, y;
+    //
+    if(spec_angle >= 0 && spec_angle < 90){
+        x = x_width + (distance * cos(spec_angle));
+        y = (distance * sin(spec_angle));
+    }
+    else if(spec_angle >= 90 && spec_angle < 180){
+        x = x_width - (distance * cos(spec_angle));
+        y = (distance * sin(spec_angle));  
+    }
+    else{
+        printf("ERROR:\tInvalid Angle\n");
+        //return "ERROR:\tInvalid Angle\n";
+    }
+    //
+    if(x >= 0 && x <=300 && y >= 200 && y <= 500){
+        //
+        std::string pad = "";
+        std::string angle_str = std::to_string(spec_angle);
+        //
+        // pad the angle string
+        if(angle_str.length() == 2 || angle_str.length() == 1){
+            pad = "0";
+        }
+        char const *angle_chstr = (pad + angle_str).c_str();
+        //
+        std::string distance_str = std::to_string(distance);
+        char const *distance_chstr = distance_str.substr(0, 7).c_str();
+        //
+        std::string x_str = std::to_string(x);
+        char const *x_chstr = x_str.substr(0, 7).c_str();
+        std::string y_str = std::to_string(y);
+        char const *y_chstr = y_str.substr(0, 7).c_str();
+        //
+        printf("Angle: %s\t-\tDist: %s\t-\t(X: %s\t-\tY: %s)\n", angle_chstr, distance_chstr, x_chstr, y_chstr);
+        //
+        // string to be sent of length 27
+        //std::string packet = pad + angle_str + ' ' + distance_str.substr(0, 7) + ' ' + x_str.substr(0, 7) + ' ' + y_str.substr(0, 7) + '\n';
+        //char const *packet_chstr = packet.c_str();
+        // printf("%s\n", packet_chstr);
+        //
+        // fflush(stdout);
+        //
+        // returning string to be sent to pynq board
+        // transmit character serially on port
+        serialPuts(serial_port, angle_chstr);
+        serialPutchar(serial_port, '/');
+        serialPuts(serial_port, distance_chstr);
+        serialPutchar(serial_port, '/');
+        serialPuts(serial_port, x_chstr);
+        serialPutchar(serial_port, '/');
+        serialPuts(serial_port, y_chstr);
+        serialPutchar(serial_port, '/');
+        serialPutchar(serial_port, '/');
+        serialPutchar(serial_port, '/');
+    }
+}
