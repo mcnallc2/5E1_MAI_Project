ceebe7920baa0a6394f13a0b20fec7d79f71b238
mcnallc2 <mcnallc2@tcd.ie>
2021-02-02 11:48:47 +0000

    updating project

--- vivado/hardware_system/arm_control/arm_control.srcs/sources_1/bd/us_arm_control/hdl/us_arm_control_wrapper.v
+++ vivado/hardware_system/arm_control/arm_control.srcs/sources_1/bd/us_arm_control/hdl/us_arm_control_wrapper.v
@@ -1,16 +1,3 @@
-//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
-//--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2018.3 (lin64) Build 2405991 Thu Dec  6 23:36:41 MST 2018
-//Date        : Mon Feb  1 10:49:15 2021
-//Host        : noodle-box running 64-bit Ubuntu 18.04.5 LTS
-//Command     : generate_target us_arm_control_wrapper.bd
-//Design      : us_arm_control_wrapper
-//Purpose     : IP block netlist
-//--------------------------------------------------------------------------------
-`timescale 1 ps / 1 ps
-
-
-
 module us_arm_control_wrapper
    (sysclk,
     reset,
